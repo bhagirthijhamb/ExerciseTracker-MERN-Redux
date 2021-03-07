b9226cb26acb5c3193d80383df0a1271e4dc22e9
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-03-06 21:49:45 -0500

    Server setup

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+// const connectDB = require('./config/db');
+// const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+
+// App Setup
+// Getting the express work the way we want it to
+
+
+// Server Setup
+// Getting our express application to talk tot he outside world
