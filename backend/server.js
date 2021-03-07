91ad66b9ccdc9b1937d10e1bd5f2d5144f4f842c
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-03-07 09:25:43 -0500

    Server listening on port 3090

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 // const connectDB = require('./config/db');
 // const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+const app = express();
+app.use(express.json());
 
 // App Setup
 // Getting the express work the way we want it to
 
 
 // Server Setup
-// Getting our express application to talk tot he outside world
+// Getting our express application to talk to the outside world
+const PORT = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(PORT, console.log(`Server listening on port ${PORT}`))
